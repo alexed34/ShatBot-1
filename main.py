e8d263445ec395fc982d18ff28184252060b7791
alexed34 <alexed34@gmail.com>
2020-05-28 11:10:36 +0400

    2 step

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import requests
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-Token = os.getenv('Authorization')
-url = 'https://dvmn.org/api/user_reviews/'
-headers = {'Authorization': Token}
+TOKEN = os.getenv('Authorization')
+headers = {'Authorization': TOKEN}
+params = {'timestamp': '1590647860'}
 
-def get_checks(url):
+
+def get_checks():
+    url = 'https://dvmn.org/api/user_reviews/'
     response = requests.get(url, headers=headers)
     response.raise_for_status
     return response
-    
+
+
+def get_checks_long_polling():
+    long_polling_url = 'https://dvmn.org/api/long_polling/'
+    response = requests.get(long_polling_url, headers=headers, params=params)
+    response.raise_for_status
+    return response
+
+
 def main():
-    checks = get_checks(url).text
+    checks = get_checks().text
     print(checks)
 
 
