5ab3161c674cebdd87fde50a2e77b55763f87e7f
alexed34 <alexed34@gmail.com>
2020-06-15 15:42:33 +0400

    finich

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
-logger = logging.getLogger(__name__)
+loggert_my = logging.getLogger('логер отладки')
 
 
 def get_checks(headers):
     url = 'https://dvmn.org/api/user_reviews/'
     response = requests.get(url, headers=headers)
-    logger.info(f'Ответ сервера: {response.status_code}')
+    loggert_my.info(f'Ответ сервера: {response.status_code}')
     response.raise_for_status()
     json_data = response.json()
     if 'error' in json_data:
@@ -26,8 +26,8 @@ def get_checks(headers):
 
 def get_checks_long_polling(header, params):
     url = 'https://dvmn.org/api/long_polling/'
-    response = requests.get(url, headers=header, params=params, timeout=2)
-    logger.info(f'Ответ сервера: {response.status_code}')
+    response = requests.get(url, headers=header, params=params)
+    loggert_my.info(f'Ответ сервера: {response.status_code}')
     response.raise_for_status()
     json_data = response.json()
     if 'error' in json_data:
@@ -35,59 +35,36 @@ def get_checks_long_polling(header, params):
     return response
 
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--devman', default=os.getenv('DEVMAN_TOKEN'))
-    parser.add_argument('-t', '--telegram', default=os.getenv('TELEGRAM_TOKEN'))
-    parser.add_argument('-c', '--chat', default=os.getenv('TG_CHAT_ID'))
-    parser.add_argument('-f', '--file', default='simple.log')
-    return parser
-
 def heroku_create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--devman', default=os.environ['DEVMAN_TOKEN'])
-    parser.add_argument('-t', '--telegram', default=os.environ['TELEGRAM_TOKEN'])
+    parser.add_argument('-t', '--telegram',
+                        default=os.environ['TELEGRAM_TOKEN'])
     parser.add_argument('-c', '--chat', default=os.environ['TG_CHAT_ID'])
     parser.add_argument('-f', '--file', default='simple.log')
     return parser
 
 
 def send_message_bot(TELEGRAM_TOKEN, TG_CHAT_ID, text):
-    logger.info('Подключение к telegram-боту')
+    loggert_my.info('Подключение к telegram-боту')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    logger.info('Отправка сообщения telegram-боту')
+    loggert_my.info('Отправка сообщения telegram-боту')
     bot.send_message(
         chat_id=TG_CHAT_ID,
-        parse_mode=telegram.ParseMode.HTML,
         text=text
     )
-
-def telegram_logger_bot(TELEGRAM_TOKEN, TG_CHAT_ID, text):
-
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    bot.send_message(
-        chat_id=TG_CHAT_ID,
-        parse_mode=telegram.ParseMode.HTML,
-        text='Bot упал с ошибкой'
-    )
-    bot.send_message(
-        chat_id=TG_CHAT_ID,
-        parse_mode=telegram.ParseMode.HTML,
-        text=text
-    )
-
 
 
 def create_log(file_path):
-    logger.setLevel(logging.DEBUG)
+    loggert_my.setLevel(logging.DEBUG)
     f_format = logging.Formatter(
         '%(asctime)s - %(name)s -  %(lineno)d - %(levelname)s - %(message)s')
     file_handler = RotatingFileHandler(file_path, maxBytes=10000, backupCount=2)
     file_handler.setFormatter(f_format)
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(f_format)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    loggert_my.addHandler(file_handler)
+    loggert_my.addHandler(console_handler)
 
 
 def get_checked_text(check):
@@ -103,33 +80,39 @@ def get_checked_text(check):
 
 
 def main():
-    #parser = create_parser()
     parser = heroku_create_parser()
     namespace = parser.parse_args()
     TELEGRAM_TOKEN = namespace.telegram
     DEWMAN_TOKEN = namespace.devman
     TG_CHAT_ID = namespace.chat
-
     create_log(namespace.file)
-
     header = {'Authorization': f'Token {DEWMAN_TOKEN}'}
     params = {'timestamp': ''}
 
-    text = 'Bot запущен'
-    send_message_bot(TELEGRAM_TOKEN, TG_CHAT_ID, text)
+    bot_log = telegram.Bot(token=TELEGRAM_TOKEN)
+
+    class MyLogsHandler(logging.Handler):
+        def emit(self, record):
+            log_entry = self.format(record)
+            bot_log.send_message(chat_id=TG_CHAT_ID, text=log_entry)
+
+    logger = logging.getLogger('Bot_loger')
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(MyLogsHandler())
+    logger.warning('bot start')
 
     while True:
         try:
-            logger.info('________Отправляем запрос сайту Heroku_________')
+            loggert_my.info('________Отправляем запрос сайту Heroku_________')
             check = get_checks_long_polling(header, params).json()
-            logger.info(f'Проверяем json ')
+            loggert_my.info(f'Проверяем json ')
             if check['status'] == 'timeout':
-                logger.info(
+                loggert_my.info(
                     f'получен ответ: "status = timeout" проверенных работ нет')
                 timestamp = check['timestamp_to_request']
                 params = {'timestamp': timestamp}
             elif check['status'] == 'found':
-                logger.info(
+                loggert_my.info(
                     f'Получен ответ: "status = found" работа проверенна{check}')
                 timestamp = check['last_attempt_timestamp']
                 params = {'timestamp': timestamp}
@@ -137,18 +120,16 @@ def main():
                 send_message_bot(TELEGRAM_TOKEN, TG_CHAT_ID, text)
 
         except requests.exceptions.ReadTimeout:
-            logger.exception('ReadTimeout')
-
-            telegram_logger_bot(TELEGRAM_TOKEN, TG_CHAT_ID, logger.exception('ReadTimeout'))
-            time.sleep(5)
             pass
 
         except requests.exceptions.ConnectionError:
             logger.exception(f'No internet or wrong url ')
+            loggert_my.exception(f'No internet or wrong url ')
             time.sleep(5)
 
         except Exception:
             logger.exception(f'Ошибка ')
+            loggert_my.exception(f'Ошибка ')
             return False
 
 
